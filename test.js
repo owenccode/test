870d2becc66939039aa4c160e827472167330ed1
owenccode <135147597+owenccode@users.noreply.github.com>
2023-05-31 13:28:06 -0500

    test.js

--- test.js
+++ test.js
@@ -52,7 +52,11 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
+  
   document.querySelectorAll('.window-weddings').forEach(trigger => {
     trigger.addEventListener('click', function () {
       document.querySelectorAll('.window-weddings').forEach(target => target.classList.add('up'));
@@ -94,6 +98,63 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   document.querySelectorAll('.window-inquire').forEach(trigger => {
+    trigger.addEventListener('click', function () {
+      document.querySelectorAll('.window-inquire').forEach(target => target.classList.add('up'));
+      document.querySelectorAll('.window-weddings').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-videos').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-photography').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-animation').forEach(target => target.classList.remove('up'));
+    });
+  });
+});
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  document.querySelectorAll('.start-wedding').forEach(trigger => {
+    trigger.addEventListener('click', function () {
+      document.querySelectorAll('.window-weddings').forEach(target => target.classList.add('up'));
+      document.querySelectorAll('.window-photography').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-videos').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-animation').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-inquire').forEach(target => target.classList.remove('up'));
+    });
+  });
+
+  document.querySelectorAll('.start-video').forEach(trigger => {
+    trigger.addEventListener('click', function () {
+      document.querySelectorAll('.window-videos').forEach(target => target.classList.add('up'));
+      document.querySelectorAll('.window-weddings').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-photography').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-animation').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-inquire').forEach(target => target.classList.remove('up'));
+    });
+  });
+
+  document.querySelectorAll('.start-photography').forEach(trigger => {
+    trigger.addEventListener('click', function () {
+      document.querySelectorAll('.window-photography').forEach(target => target.classList.add('up'));
+      document.querySelectorAll('.window-weddings').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-videos').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-animation').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-inquire').forEach(target => target.classList.remove('up'));
+    });
+  });
+
+  document.querySelectorAll('.start-animation').forEach(trigger => {
+    trigger.addEventListener('click', function () {
+      document.querySelectorAll('.window-animation').forEach(target => target.classList.add('up'));
+      document.querySelectorAll('.window-inquire').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-weddings').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-videos').forEach(target => target.classList.remove('up'));
+      document.querySelectorAll('.window-photography').forEach(target => target.classList.remove('up'));
+    });
+  });
+
+  document.querySelectorAll('.start-inquire').forEach(trigger => {
     trigger.addEventListener('click', function () {
       document.querySelectorAll('.window-inquire').forEach(target => target.classList.add('up'));
       document.querySelectorAll('.window-weddings').forEach(target => target.classList.remove('up'));
